272b456570f09b92d26e2df66c456edb77df39b3
yeol <yeol8810@gmail.com>
2022-08-27 17:16:14 +0900

    react + typescript props
    react+typescript props 사용 연습.

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,8 @@ import Circle from './Circle';
 function App() {
   return (
     <div>
-      <Circle />
+      <Circle bgColor='teal' />
+      <Circle bgColor='tomato' />
     </div>
   );
 }
--- src/Circle.tsx
+++ src/Circle.tsx
@@ -1,9 +1,21 @@
 import React from 'react';
 import styled from 'styled-components';
 
-const Container = styled.div``;
-function Circle() {
-  return <Container />;
+interface ContainerProps {
+  bgColor: string;
+}
+const Container = styled.div<ContainerProps>`
+  width: 100px;
+  height: 100px;
+  background-color: ${(props) => props.bgColor};
+`;
+
+interface CircleProps {
+  bgColor: string;
+}
+
+function Circle({ bgColor }: CircleProps) {
+  return <Container bgColor={bgColor} />;
 }
 
 export default Circle;
