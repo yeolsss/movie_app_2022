ac82c999996af359e51cd8427b85a4db6fbe3f83
yeol <yeol8810@gmail.com>
2022-08-27 21:02:49 +0900

    react + typescript forms
    react + typescript에서 fomrs 사용

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,28 @@
-import React from 'react';
-import Circle from './Circle';
+import React, { useState } from 'react';
 
 function App() {
+  const [userName, setUserName] = useState('');
+  const onChange = (event: React.FormEvent<HTMLInputElement>) => {
+    const {
+      currentTarget: { value },
+    } = event;
+    setUserName(value);
+  };
+  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    console.log('hello', userName);
+  };
   return (
     <div>
-      <Circle bgColor='teal' borderColor='black' />
-      <Circle bgColor='tomato' text='send Text' />
+      <form onSubmit={onSubmit}>
+        <input
+          value={userName}
+          onChange={onChange}
+          type='text'
+          placeholder='username'
+        />
+        <button>Log in</button>
+      </form>
     </div>
   );
 }
