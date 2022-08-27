2fc8662a1b6368fee423af9ebf3303135bd9c9e5
yeol <yeol8810@gmail.com>
2022-08-27 18:17:22 +0900

    react + typescript optional props
    typescript를 사용한 optional props사용

--- src/Circle.tsx
+++ src/Circle.tsx
@@ -3,19 +3,28 @@ import styled from 'styled-components';
 
 interface ContainerProps {
   bgColor: string;
+  borderColor: string;
 }
 const Container = styled.div<ContainerProps>`
   width: 100px;
   height: 100px;
   background-color: ${(props) => props.bgColor};
+  border-radius: 100px;
+  border: 1px solid ${(props) => props.borderColor};
 `;
 
 interface CircleProps {
   bgColor: string;
+  borderColor?: string;
+  text?: string;
 }
 
-function Circle({ bgColor }: CircleProps) {
-  return <Container bgColor={bgColor} />;
+function Circle({ bgColor, borderColor, text = 'hello' }: CircleProps) {
+  return (
+    <Container bgColor={bgColor} borderColor={borderColor ?? bgColor}>
+      {text}
+    </Container>
+  );
 }
 
 export default Circle;
